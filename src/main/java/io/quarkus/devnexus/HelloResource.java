8b589d3cd04229de8b002b10e6841d99feaa3660
Jeremy Davis <jeremyrdavis@mac.com>
2020-02-18 15:30:33 -0500

    initial

---
+++ src/main/java/io/quarkus/devnexus/HelloResource.java
@@ -0,0 +1,20 @@
+package io.quarkus.devnexus;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Optional;
+
+@Path("/hello")
+public class HelloResource {
+
+    final Optional<String> hostname = Optional.ofNullable(System.getenv().get("HOSTNAME"));
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+
+        return "Hello, Devnexus! " + hostname.orElse("\nfrom Quarkus on an unknown server");
+    }
+}
